50e30d20c71a7b4048af2137995dcc3e20c3c383
sejutisarkar <sejuti.dancinggirl8@gmail.com>
2018-09-02 11:20:45 +0530

    changes fixed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
+import { createHttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-link';
 import { ApolloProvider } from 'react-apollo';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import {setContext} from 'apollo-link-context';
 
 import 'semantic-ui-css/semantic.min.css';
 import Routes from './routes';
@@ -13,11 +15,76 @@ import registerServiceWorker from './registerServiceWorker';
 //   uri: 'http://localhost:8081/graphql',
 // });
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8000/graphql',
+});
+
+// const authLink = setContext((_, {headers}) => {
+//   //get the token from localStorage
+//   const token = localStorage.getItem('token');
+//   const refreshToken = localStorage.getItem('refreshToken')
+//   //return the headers to the context so that httplink can read them
+//   return {
+//     ...headers,
+//     authorization: token ? `Bearer ${token}` : "",
+//   }
+// });
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || null,
+    }
+  }));
+
+  return forward(operation);
+});
+
+
+const otherMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('refreshToken') || null,
+    }
+  }));
+
+  return forward(operation);
+})
+
+const afterwareLink = new ApolloLink((operation, forward) => {
+  return forward(operation).map(response => {
+    const context = operation.getContext();
+    const { response: { headers } } = context;
+
+    if (headers) {
+      const token = headers.get("x-token");
+      const refreshToken = headers.get("x-refresh-token");
+
+      if (token) {
+        localStorage.setItem("token", token);
+      }
+
+      if (refreshToken) {
+        localStorage.setItem("refreshToken", refreshToken);
+      }
+
+    }
+    return response;
+  });
+});
+
 const client = new ApolloClient({
-  link: new HttpLink({
-    uri: 'http://localhost:8000/graphql',
-  }),
-  cache: new InMemoryCache()
+  // link: from([
+  //   authMiddleware,
+  //   otherMiddleware,
+  //   httpLink
+  // ]),
+  link: ApolloLink.from([authMiddleware, otherMiddleware, httpLink]),
+   cache: new InMemoryCache()
 });
 
 const App = (
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './home';
 import Register from './register';
 import Login from './login';
+import CreateTeam from './createTeam';
 
 export default () => (
   <BrowserRouter>
@@ -11,6 +12,7 @@ export default () => (
       <Route path="/" exact component={Home} />
       <Route path="/login" exact component={Login} />
       <Route path="/register" exact component={Register} />
+      <Route path="/createTeam" exact component={CreateTeam} />
     </Switch>
   </BrowserRouter>
 );
